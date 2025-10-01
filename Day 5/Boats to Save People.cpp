3828143d6af775f2d1c52e58cd9f0188da45c621
Harsh Kushwaha <harsh2004great@gmail.com>
2025-10-01 19:31:15 +0000

    4

---
+++ Day 5/Boats to Save People.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int numRescueBoats(vector<int>& people, int limit) {
+        sort(people.begin(), people.end());
+        int ans =0;
+        int s =0, e= people.size()-1;
+        while(s<=e){
+            if(people[s]+people[e] <= limit){
+                s++;
+                e--;
+            }
+            else{
+                e--;
+            }
+            ans++;
+        }
+        return ans;
+    }
+};
